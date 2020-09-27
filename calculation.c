e6f9cb3ed20fc9a2faafe1c908c988000547ed06
nortonlyr <nortonlyr@gmail.com>
2020-09-27 19:50:03 -0400

    updated

--- calculation.c
+++ calculation.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+const int N = 3;
+
 int main(void)
 {
-    int scores[3];
+    int scores[N];
     scores[0] = 72;
     scores[1] = 73;
     scores[2] = 33;
 
-    printf("Average: %i\n", (scores[0] + scores[1] + scores[2]) / 3);
+    printf("Average: %i\n", (scores[0] + scores[1] + scores[2]) / N);
 }
